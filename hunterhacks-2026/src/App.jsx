afb6c82164e621a4fe0147004fdfa21a32bf7ddd
Emily Klapper <104882859+emiklap@users.noreply.github.com>
2026-04-25 01:54:21 -0400

    Update BASTA image source in App.jsx

--- hunterhacks-2026/src/App.jsx
+++ hunterhacks-2026/src/App.jsx
@@ -674,7 +674,7 @@ function App() {
                   className="sponsor-card card-2"
                 />
                 <img
-                  src="/BASTA_Card.png"
+                  src="/BASTA_Card_new.png"
                   alt="BASTA"
                   className="sponsor-card card-3"
                 />
